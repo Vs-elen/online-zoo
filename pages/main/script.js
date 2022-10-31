d479f5fd4130e6c71d609915ea5e0c6e2ba3306a
Vs-elen <elenvsiakikh@gmail.com>
2022-10-31 23:36:35 +0300

    feat: add footer

---
+++ pages/main/script.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper(".testimonials__table", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },    
+  });
